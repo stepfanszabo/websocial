914bd8e07bada1f58cdd7833eea9d512cb375a05
Stefan <szstefan137@gmail.com>
2018-05-30 19:32:27 +0200

    404

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAxKmMVRHhraDZvm7ZNSFnrwX7O88egnAk",
+    authDomain: "abweb-6f6ae.firebaseapp.com",
+    databaseURL: "https://abweb-6f6ae.firebaseio.com",
+    projectId: "abweb-6f6ae",
+    storageBucket: "abweb-6f6ae.appspot.com",
+    messagingSenderId: "732664259502"
+  }
 };
